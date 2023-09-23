65d8154324fbcc60ba0c2f0c7d2dff9330767aad
emilyyluo <emily.luo51@gmail.com>
2023-09-23 15:30:10 -0400

    home navigation bar colour changes

--- shieldher/lib/home.dart
+++ shieldher/lib/home.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:shieldher/hometab.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const DefaultTabController(
@@ -8,18 +11,21 @@ class HomePage extends StatelessWidget {
       child: Scaffold(
         body: TabBarView(
           children: [
+            // Placeholder(),
+            HomeTab(),
             Placeholder(),
             Placeholder(),
-            Placeholder(),
-            // new HomePage(),
-            // new ContactList(),
-            // new EditPage(),
+            // HomeTab(
+            // ),
+            // ContactsTab(),
+            // SettingsTab(),
           ],
         ),
         bottomNavigationBar: TabBar(
             tabs: [
               Tab(
                 icon: Icon(Icons.home),
+                
               ),
               Tab(
                 icon: Icon(Icons.list),
@@ -28,7 +34,27 @@ class HomePage extends StatelessWidget {
                 icon: Icon(Icons.settings),
               ),
             ],
+            indicatorColor: Color.fromARGB(255, 232, 217, 193),
+            unselectedLabelColor: Color.fromARGB(255, 232, 217, 193),
+
         ),
+        backgroundColor: Color.fromARGB(255, 203, 133, 137),
+      ),
+    );
+  }
+}
+
+class HomeTab extends StatelessWidget {
+  const HomeTab({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color.fromARGB(255, 203, 133, 137),
+      child: FloatingActionButton(
+        onPressed: () {}
+        
       ),
     );
   }

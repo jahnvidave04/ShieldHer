5333ea92ab5fc353446a3e86982dbba9d772e53f
Brianna <132308375+briannale935@users.noreply.github.com>
2023-09-23 20:11:57 -0400

    send location button

--- shieldher/lib/phonecall.dart
+++ shieldher/lib/phonecall.dart
@@ -9,7 +9,7 @@ class PhoneCall extends StatelessWidget {
       ),
       body: Center ( 
         child: PhoneScreen ()
-      )
+      ),
     ); 
   }
 }
@@ -17,13 +17,50 @@ class PhoneCall extends StatelessWidget {
 class PhoneScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('assets/phonecall.png'),
-          fit: BoxFit.cover,
+    return Stack(
+      children: [ 
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/phonecall.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
         ),
-      ),
+        Positioned (
+          bottom: 10, 
+          right: 16, 
+          child: ElevatedButton ( 
+            onPressed: () { 
+              showDialog(
+                context: context,
+                builder: (BuildContext context) { 
+                  return AlertDialog(
+                    title: Text("Location Sent"),
+                    content: Text("Your location has been sent to your emergency contacts"),
+                    actions: [
+                      TextButton(
+                        child: Text("Close", 
+                          style: TextStyle (
+                              color: Colors.black, 
+                            )),
+                        onPressed: () { 
+                          Navigator.of(context).pop(); 
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+            child: Text("Send Location"),
+            style: ButtonStyle ( 
+              backgroundColor: MaterialStateProperty.all<Color>(Colors.red.withOpacity(0.7)),
+              foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+            )
+          ),
+        ),
+      ],
     );
   }
 }
